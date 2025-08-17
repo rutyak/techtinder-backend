3569b2d13171aa7659734be4739b33239656a7cb
rutyak <rutikkhandekar12@gmail.com>
2025-08-17 17:56:48 +0530

    webhook-bug-fixed

--- src/router/paymentRouter.js
+++ src/router/paymentRouter.js
@@ -60,7 +60,7 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 
 paymentRouter.post("/payment/webhook", async (req, res) => {
   try {
-    const webhookSignature = req.headers["x-razorpay-signature"];
+    const webhookSignature = req.get("x-razorpay-signature");
 
     const isWebhookValid = validateWebhookSignature(
       JSON.stringify(req.body),
